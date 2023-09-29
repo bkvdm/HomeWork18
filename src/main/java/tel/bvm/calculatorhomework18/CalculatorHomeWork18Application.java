0e35953687a26d4ee77775f37ca61d6d9c9cff81
bkvdm <vadim@991199.ru>
2023-09-29 19:26:43 +0300

    Домашнее задание №18 (Калькулятор). Тема: Вводное в Maven и Spring Boot + web.

---
+++ src/main/java/tel/bvm/calculatorhomework18/CalculatorHomeWork18Application.java
@@ -0,0 +1,13 @@
+package tel.bvm.calculatorhomework18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorHomeWork18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorHomeWork18Application.class, args);
+	}
+
+}
---
+++ src/test/java/tel/bvm/calculatorhomework18/CalculatorHomeWork18ApplicationTests.java
@@ -0,0 +1,13 @@
+package tel.bvm.calculatorhomework18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorHomeWork18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
